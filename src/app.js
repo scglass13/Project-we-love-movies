7aacc5edd63a74a6d3676be0e7abe51c213b1888
Sean Glass <scglass13@gmail.com>
2021-12-13 17:37:30 -0600

    fixed migration issues

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 
+app.use(morgan("dev"));
 
+const getMovies = (req, res, next) => {
+    res.json(data);
+};
+
+app.get("/movies", getMovies);
 
 module.exports = app;
--- src/db/migrations/20211210190505_reviews.js
+++ src/db/migrations/20211210190505_reviews.js
@@ -3,11 +3,13 @@ exports.up = function (knex) {
     table.integer("review_id").primary();
     table.text("content");
     table.integer("score");
+    table.integer("movie_id").unsigned().notNullable();
     table
       .foreign("movie_id")
       .references("movie_id")
       .inTable("movies")
       .onDelete("cascade");
+    table.integer("critic_id").unsigned().notNullable();
     table
       .foreign("critic_id")
       .references("critic_id")
